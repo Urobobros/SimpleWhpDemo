0fc2814a0fbfbcb55c1942098d225d9c4351b87d
Urobobros <martin.burianek1987@gmail.com>
2025-07-26 23:18:43 +0200

    Remove duplicate port logging

--- SimpleWhpDemo/main.c
+++ SimpleWhpDemo/main.c
@@ -953,7 +953,6 @@ HRESULT SwEmulatorIoCallback(IN PVOID Context, IN OUT WHV_EMULATOR_IO_ACCESS_INF
                printf("Input from port 0x%04X (%s) is not implemented!\n", IoAccess->Port, GetPortName(IoAccess->Port));
                return E_NOTIMPL;
        }
-       PORT_LOG("OUT port 0x%04X, size %u, value 0x%02X\n", IoAccess->Port, IoAccess->AccessSize, IoAccess->Data);
         if (IoAccess->Port <= 0x0007)
         {
                 PortLogIoWithTag(IoAccess, "dma_write");
